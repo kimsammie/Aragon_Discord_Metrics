f431be275ee1f8b68512ebc1f10c2b19949d981f
kimsammie <65345731+kimsammie@users.noreply.github.com>
2022-03-06 14:19:40 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -73,8 +73,8 @@ def write():
 	# below is authorization from my discord login
 
 	st.sidebar.write('Choose a week')
-	var1 = st.sidebar.number_input('Enter the the start of date the week (e.g., 2022-02-21)')
-	st.table(df1.head(int(var1)))
+	date_ofweek = st.sidebar.number_input('Enter the the start of date the week (e.g., 2022-02-21)')
+	# st.table(df1.head(int(var1)))
 
 
 	st.sidebar.write('Choose the Discord channel')
@@ -90,6 +90,11 @@ def write():
 		# var1 = st.sidebar.number_input('Enter the number of top pairs (max 50)', max_value = 50)
 		# st.table(df1.head(int(var1)))
 		#
+
+
+	st.sidebar.write('Number of Topics')
+	numberof_topics = st.sidebar.number_input('Enter the number of topics')
+	# st.table(df1.head(int(var1)))
 
 
 	def retrieve_messages1(channelid):
@@ -124,6 +129,146 @@ def write():
 	df = pd.DataFrame(data)
 	df.sort_values('timestamp', ascending=False, inplace=True)
 	df.timestamp = pd.to_datetime(df.timestamp)
+
+	# add additional data
+
+	while len(df) < 5000:  # or use before/after timestamp
+		latestid = df.tail(1)['id'].values[0]
+		newdata = retrieve_messages2(channel_num, latestid)
+		df1 = pd.DataFrame(newdata)
+		df1.timestamp = pd.to_datetime(df1.timestamp)
+		df = pd.concat([df, df1])  # expand the database
+		df.sort_values('timestamp', ascending=False, inplace=True)
+	# latestdate = df.tail(1)['timestamp'].values[0]
+
+	df = df.reset_index(drop=True)  # if not set to a variable it won't reset
+
+	df['timestamp'] = df['timestamp'].dt.date
+	start_date = pd.to_datetime(date_ofweek).date()
+	end_date = pd.to_datetime('2022-02-27').date()
+	one_week = (df['timestamp'] > start_date) & (df['timestamp'] <= end_date)
+	df_1wk = df.loc[one_week]
+
+	#Tokenize Sentences and Clean
+	def sent_to_words(sentences):
+		for sent in sentences:
+			sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+			sent = re.sub('\s+', ' ', sent)  # remove newline chars
+			sent = re.sub("\'", "", sent)  # remove single quotes
+			sent = gensim.utils.simple_preprocess(str(sent),
+												  deacc=True)  # split the sentence into a list of words. deacc=True option removes punctuations
+			yield (sent)
+	# Convert to list
+	data = df.content.values.tolist()
+	data_words = list(sent_to_words(data))
+
+
+	# Build the Bigram, Trigram Models and Lemmatize
+	bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)  # higher threshold fewer phrases.
+	trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+	bigram_mod = gensim.models.phrases.Phraser(bigram)
+	trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+	# !python3 -m spacy download en  # run in terminal once
+	def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+		"""Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+		texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+		texts = [bigram_mod[doc] for doc in texts]
+		texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+		texts_out = []
+		nlp = spacy.load('en', disable=['parser', 'ner'])
+		for sent in texts:
+			doc = nlp(" ".join(sent))
+			texts_out.append([token.lemma_ for token in doc if
+							  token.pos_ in allowed_postags])  # to its root form, keeping only nouns, adjectives, verbs and adverbs
+		# remove stopwords once more after lemmatization
+		texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]
+		return texts_out
+	data_ready = process_words(data_words)  # processed Text Data!
+
+	#build the topic model
+	# To build the LDA topic model using LdaModel(), need the corpus and the dictionary.
+	# Create Dictionary
+	id2word = corpora.Dictionary(data_ready)
+
+	# Create Corpus: Term Document Frequency
+	corpus = [id2word.doc2bow(text) for text in data_ready]
+
+	# Build LDA model
+	lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+												id2word=id2word,
+												num_topics=numberof_topics,
+												random_state=100,
+												update_every=1,
+												chunksize=10,
+												passes=10,
+												alpha='symmetric',
+												iterations=100,
+												per_word_topics=True)
+
+	pprint(lda_model.print_topics())  # The trained topics (keywords and weights)
+
+	# What is the most dominant topic and its percentage contribution in each document
+	# In LDA models, each document is composed of multiple topics. But, typically only one of the topics is dominant.
+	# Below extracts the dominant topic for each sentence and shows the weight of the topic and the keywords.
+	# It shows which document belongs predominantly to which topic.
+
+	def format_topics_sentences(ldamodel=None, corpus=corpus, texts=data):
+		# Init output
+		sent_topics_df = pd.DataFrame()
+
+		# Get main topic in each document
+		for i, row_list in enumerate(ldamodel[corpus]):
+			row = row_list[0] if ldamodel.per_word_topics else row_list
+			# print(row)
+			row = sorted(row, key=lambda x: (x[1]), reverse=True)
+			# Get the Dominant topic, Perc Contribution and Keywords for each document
+			for j, (topic_num, prop_topic) in enumerate(row):
+				if j == 0:  # => dominant topic
+					wp = ldamodel.show_topic(topic_num)
+					topic_keywords = ", ".join([word for word, prop in wp])
+					sent_topics_df = sent_topics_df.append(
+						pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords]), ignore_index=True)
+				else:
+					break
+		sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+		# Add original text to the end of the output
+		contents = pd.Series(texts)
+		sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+		return (sent_topics_df)
+
+	df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data_ready)
+
+	# Format
+	df_dominant_topic = df_topic_sents_keywords.reset_index()
+	df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+	df_dominant_topic.head(10)
+
+	# the most representative sentence for each topic
+
+	# Get samples of sentences that most represent a given topic.
+	# Display setting to show more characters in column
+	pd.options.display.max_colwidth = 100
+
+	sent_topics_sorteddf_mallet = pd.DataFrame()
+	sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+	for i, grp in sent_topics_outdf_grpd:
+		sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet,
+												 grp.sort_values(['Perc_Contribution'], ascending=False).head(1)],
+												axis=0)
+
+	# Reset Index
+	sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+	# Format
+	sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Representative Text"]
+
+	# Show
+	sent_topics_sorteddf_mallet.head(10)
+
+
 
 	# 	st.markdown(
 	# """
