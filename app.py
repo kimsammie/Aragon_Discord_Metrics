47a6bf2fde2ca8246722b78eb7924f907728f9d8
kimsammie <65345731+kimsammie@users.noreply.github.com>
2022-03-09 12:27:13 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -124,7 +124,8 @@ def write():
 	df.sort_values('timestamp', ascending=False, inplace=True)
 	df.timestamp = pd.to_datetime(df.timestamp)
 
-	latestdate = df.tail(1)['timestamp'].values[0].date()
+	latestdate = df.tail(1)['timestamp'].values[0]
+	latestdate = pd.to_datetime(latestdate).date()
 
 	st.write('processing one week', latestdate)
 
