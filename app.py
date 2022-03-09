109cd6af6bb517b3bf670297005f5d0d4528999f
kimsammie <65345731+kimsammie@users.noreply.github.com>
2022-03-09 13:10:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -306,14 +306,14 @@ def write():
 
 	# st.write('Topics:', topics)
 
-	fig, axes = plt.subplots(1, 3, figsize=(10, 10), sharex=True, sharey=True)
+	fig, axes = plt.subplots(1, 4, figsize=(10, 10), sharex=True, sharey=True)
 
 	for i, ax in enumerate(axes.flatten()):
 		fig.add_subplot(ax)
 		topic_words = dict(topics[i][1])
 		cloud.generate_from_frequencies(topic_words, max_font_size=300)
 		plt.gca().imshow(cloud)
-		plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+		plt.gca().set_title('Topic ' + str(i+1), fontdict=dict(size=16))
 		plt.gca().axis('off')
 
 	plt.subplots_adjust(wspace=0, hspace=0)
