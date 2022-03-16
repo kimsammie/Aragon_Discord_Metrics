524c7c69be323fe5fce643f1cd33afb45d0b2985
kimsammie <65345731+kimsammie@users.noreply.github.com>
2022-03-16 15:31:53 -0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,24 +5,11 @@ import app
 
 st.set_page_config(layout="wide")
 
-# PAGES = {
-#
-# 	"Explore Existing Whitepapers in Database" : app,
-# 	"Upload New Whitepaper" : upload
-# }
-
 
 def main():
 
 	app.write()
-	# selection = st.sidebar.radio("", list(PAGES.keys()))
-	#
-	#
-	# if PAGES[selection] == app:
-	# 	app.write()
-	#
-	# elif PAGES[selection] == upload:
-	# 	upload.write()
+
 
 if __name__ == '__main__':
 	main()
